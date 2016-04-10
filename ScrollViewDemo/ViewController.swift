a853ed6c1396cfe15eb83a5d9af1e153e540ad6e
dipak2016 <macos@MACs-iMac-3.local>
2016-04-10 18:50:33 +0530

    scrollview111

--- ScrollViewDemo/ViewController.swift
+++ ScrollViewDemo/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
 
+    @IBOutlet weak var img_vw: UIImageView!
+    @IBOutlet weak var scroll_vw: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scroll_vw.delegate=self
+        scroll_vw.minimumZoomScale=0
+        scroll_vw.maximumZoomScale=4
     }
-
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView?
+    {
+        return img_vw
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
